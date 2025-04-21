36e87f70773c2849e9babcd3174e036215ffdbb2
omartazi <tasiomar@gmail.com>
2025-04-21 15:40:15 +0100

    Stored api_keys in a dictionnary

---
+++ data/raw/api_keys.py
@@ -0,0 +1,16 @@
+# Dictionary to store API keys for various data providers
+API_KEYS = {
+    "alphavantage": "XRKOI9H9AGO2VP6L",
+    "benzinga": "bz.EZ4H5ZHV36F35SIJJVSI2QTYUWIF5XVZ",
+    "biztoc": "ec6980e336msh5b06c637c1464e1p1fc068jsn39a6f8dccfac",
+    "fmp": "T0D2ZU6FW4fj6yTE7uV50YBpjK3H1CH2",
+    "fred": "d636c702a0f9e3e97f55065da983a21c",
+    "nasdaq": "pkvbNvA3JDWTsSWUKhe-",
+    "polygon": "e1PKxyENaJevoZrYDp9uNPyiFjERdYll",
+    "tiingo": "f1ac24ca3c2040a1655052b953ee03c5ac6c416b",
+}
+
+
+
+
+    
